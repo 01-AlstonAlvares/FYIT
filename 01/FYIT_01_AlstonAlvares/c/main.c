60684f36e67f5f5c944c6f788f9c63e614b48360
01-AlstonAlvares <112925234+01-AlstonAlvares@users.noreply.github.com>
2022-11-24 08:58:56 +0530

    Add files via upload

---
+++ 01/FYIT_01_AlstonAlvares/c/main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+double num1;
+double num2;
+char op;
+
+printf("Enter a number :");
+scanf("%1f", num1);
+printf("Enter a operator :");
+scanf("%s", op);
+printf("Enter a number");
+scanf("%d", num2);
+
+return 0;
+}
